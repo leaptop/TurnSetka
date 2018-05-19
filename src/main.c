760a7dd46f785bdc88bc356cf35085ecc855a31c
anaksi <anaksizlo@gmail.com>
2018-05-19 18:35:22 +0600

    Create ability to choose winners

--- src/main.c
+++ src/main.c
@@ -3,13 +3,18 @@
 #include <string.h>
 
 
+int SetPrint(char** names, int m);
+
 char** board;
 
 void FillBoard(){
 
 
-    board[0] = "hello";
-    board[1] = "bye";
+    board[0] = "very";
+    board[1] = "cool";
+    board[2] = "trpo";
+    board[3] = "project";
+
 
 }
 
@@ -27,38 +32,105 @@ void InitNames(){
     FillBoard();
 }
 
-void SetPrint(char** names, int m){
+int SetPrint(char** names, int m){
 
-int i,j,l;
+int i,j,l,k;
 
 char lines[] = "--------------------";
+k=m;
+if (m % 2 == 1) k=m-1;
 
-for (i=0; i<m; i++){
-    if (i % 2 == 1){
-        printf("%c",names[i]);
-        for (j=0;j<(20 - strlen(names[i]));j++)
+
+for (i=0; i<k; i++){
+    if (i % 2 == 0){
+        printf("%s",names[i]);
+        for (j=0;j<(15 - strlen(names[i]));j++)
             printf("-");
-        for (l=0;l<5;l++)
-        printf("\n                       I");
+        printf("|");
+
+        printf("\n               |");
+        printf("\n               |---");
+    }
+    if (i % 2 == 1){
+        printf("\n               |");
+        printf("\n");
+        printf("%s",names[i]);
+        for (j=0;j<(15 - strlen(names[i]));j++)
+            printf("-");
+        printf("|");
+        printf("\n");
+        printf("\n");
+
+
     }
 
 
+
+    }
+
+    if (m % 2 == 1){
+        printf("%s",names[m-1]);
+        for (j=0;j<(15 - strlen(names[m-1]));j++)
+            printf("-");
+
+}
+
+return 0;
+
+}
+
+void Winners(){
+int i;
+int n[2];
+printf("Choose winners in each pair! (input 1 if team 1 won, input 2 if team 2 won) \n");
+
+
+for(i=1;i<3;i++){
+printf("Winner of pair %d: \n",i);
+scanf("%d",&n[i-1]);
+
+}
+int mul=0;
+for(i=0; i<2; i++){
+    board[i]=board[n[i] - 1 + mul];
+    mul+=2;
+
+
 }
 
 
 
-
-
 }
+
 
 
 
 
 
 int main(){
+int x;
 
-InitNames();
-SetPirnt(board, 1);
+while(1){
+printf("1 - initial grid \n");
+printf("2 - choose winners \n");
+
+
+
+scanf("%d",&x);
+
+switch (x){
+
+case 1 : InitNames(); SetPrint(board,4); break;
+case 2 : InitNames(); Winners(); SetPrint(board,2);break;
+
+
+
+
+
+}
+
+
+}
 
 
 }
