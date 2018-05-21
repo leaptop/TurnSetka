ed10c638250acf914da6ce6074feb78256e65171
anaksi <anaksizlo@gmail.com>
2018-05-21 19:26:51 +0600

    Minor fix to main.c

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ char **c;
 
 
 int SetPrint(char** names, int m);
-
+void Winners();
 char** losers;
 char q = 'n';
 /*
@@ -30,12 +30,10 @@ void FillBoard(){
 
 void InitNames(){
     int i,j;
-    losers = (char**)malloc(20 * sizeof(char*));
-    for (i = 0; i < 8; i++) {
-        losers[i] = (char*)malloc(20 * sizeof(char));
-        for (j = 0; j < 8; j++) {
-            losers[i][j] = ' ';
-        }
+    c = (char**)malloc(30 * sizeof(char*));
+    for (i = 0; i < 30; i++) {
+        c[i] = (char*)malloc(30 * sizeof(char));
+
     }
 
 }
@@ -93,32 +91,39 @@ return 0;
 
 void Winners(){
 int i;
-
-int n[m % 2];
-int l[m % 2];
-printf("Will there be lower bracket? y/n \n");
-scanf ("%c",&q);
+printf("%d \n", m);
+int n[m/2];
+int l[20];
+//printf("Will there be lower bracket? y/n \n");
+//scanf ("%c",&q);
 
 
 
 printf("Choose winners in each pair! (input 1 if team 1 won, input 2 if team 2 won) \n");
+printf("%d \n", m / 2);
 
-
-for(i=1;i<(m % 2) + 1;i++){
+for(i=1;i<(m / 2) + 1;i++){
 printf("Winner of pair %d: \n",i);
 scanf("%d",&n[i-1]);
-if (n[i-1] == 1) l[i-1] = 2;
-    else l[i-1] = 1;
+//if (n[i-1] == 1) l[i-1] = 2;
+  //  else l[i-1] = 1;
 
 }
+
+
 int mul=0;
-for(i=0; i<m % 2; i++){
-    n[i]=n[n[i] - 1 + mul];
+for(i=0; i<m / 2; i++){
+    c[i]=c[n[i] - 1 + mul];
     mul+=2;
-    losers[i] = c[l[i] - 1 + mul];
+   // losers[i] = c[l[i] - 1 + mul];
 
 }
 
+if (m % 2 == 1) c[i]=c[m-1];
+
+
+if (m % 2 == 0) m=m/2;
+    else m=m/2 + 1;
 
 
 }
@@ -130,13 +135,15 @@ for(i=0; i<m % 2; i++){
 
 int main(){
 int x,s,ss;
-InitNames();
+//InitNames();
 
-char **c = (char  **)malloc(30 * sizeof(char *));
+InitNames();
+char **losers = (char  **)malloc(30 * sizeof(char *));
 
 char chr[30][20];
 
 readFile(c, chr, &m);
+
 
 for(s=0; s<m; s++){
     c[s][strlen(c[s])-1] = '\0';
