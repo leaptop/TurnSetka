749f1cecf7ed1ea54b5222a1a17397417127df42
Stepan <stepan-alekseev-87@mail.ru>
2018-05-31 04:58:09 +0700

    Formatting 3 (from visual studio)

--- src/Funcs.h
+++ src/Funcs.h
@@ -1,1 +1,8 @@
 char** readFile01(char **text, char chr[][20]);
+int SetPrint(char **names, int m);
+void Winners();
+void WinnersL();
+void WinnersLL();
+void InitNames();
+void InitLosers();
+int SetPrintL(char **names, int m);
