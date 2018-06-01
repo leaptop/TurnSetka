7766002a818b72a0ff000ba250684a948906cf36
sascvatch <stepan-alekseev-87@mail.ru>
2018-06-02 06:06:58 +0700

    Update Funcs.c

--- src/Funcs.c
+++ src/Funcs.c
@@ -1,9 +1,10 @@
-﻿#ifndef FUNCS
+#ifndef FUNCS
 #define FUNCS
 
-int checkIns(char ins[] ){
-if((ins[0] != 48 && ins[0] != 49 && ins[0] != 50) || ins[1]) {
-    return 1;
+int checkIns(char ins[])
+{
+    if ((ins[0] != 48 && ins[0] != 49 && ins[0] != 50) || ins[1]) {
+        return 1;
     } else {
         return 0;
     }
