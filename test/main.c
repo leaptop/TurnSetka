fe817d06621439fbdc7d4b35384bf4a9f8eb8303
sascvatch <stepan-alekseev-87@mail.ru>
2018-06-01 18:41:39 +0700

    the fix doesn't affect travis

--- test/main.c
+++ test/main.c
@@ -3,7 +3,14 @@
 #include <Funcs_2.h>
 #include <Funcs.h>
 
-CTEST(checkIns, Correct) {
+CTEST(checkIns, Correct1) {
+    char ins [1];
+    ins [0] = '2';
+    int result = checkIns(ins);
+    int expected = 0;
+    ASSERT_EQUAL(expected,result);
+}
+CTEST(checkIns, Correct2) {
     char ins [1];
     ins [0] = '1';
     int result = checkIns(ins);
@@ -13,7 +20,7 @@ CTEST(checkIns, Correct) {
 
 CTEST(checkIns, Incorrect1) {
     char ins [1];
-    ins [0] = 'f';
+    ins [0] = 'g';
     int result = checkIns(ins);
     int expected = 1;
     ASSERT_EQUAL(expected,result);
