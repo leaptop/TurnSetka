56a08cbf6822106ef37ef6b898b716040549d2c8
sascvatch <stepan-alekseev-87@mail.ru>
2018-06-01 18:49:17 +0700

    ritin tests

--- test/main.c
+++ test/main.c
@@ -3,20 +3,8 @@
 #include <Funcs_2.h>
 #include <Funcs.h>
 
-CTEST(checkIns, Correct1) {
-    char ins [1];
-    ins [0] = '2';
-    int result = checkIns(ins);
-    int expected = 0;
-    ASSERT_EQUAL(expected,result);
-}
-CTEST(checkIns, Correct2) {
-    char ins [1];
-    ins [0] = '1';
-    int result = checkIns(ins);
-    int expected = 0;
-    ASSERT_EQUAL(expected,result);
-}
+
+
 
 CTEST(checkIns, Incorrect1) {
     char ins [1];
@@ -33,6 +21,38 @@ CTEST(checkIns, Incorrect2) {
     int expected = 1;
     ASSERT_EQUAL(expected,result);
 }
+
+CTEST(checkIns, Incorrect3) {
+    char ins [12];
+    ins [0] = 'g', ins[1] = 'a';
+    int result = checkIns(ins);
+    int expected = 1;
+    ASSERT_EQUAL(expected,result);
+}
+
+CTEST(checkIns, Incorrect4) {
+    char ins [1];
+    ins [0] = '9';
+    int result = checkIns(ins);
+    int expected = 1;
+    ASSERT_EQUAL(expected,result);
+}
+
+
+CTEST(checkIns, Correct1) {
+    char ins [1];
+    ins [0] = '2';
+    int result = checkIns(ins);
+    int expected = 0;
+    ASSERT_EQUAL(expected,result);
+}
+CTEST(checkIns, Correct2) {
+    char ins [1];
+    ins [0] = '1';
+    int result = checkIns(ins);
+    int expected = 0;
+    ASSERT_EQUAL(expected,result);
+}//these two don't work in travis for some reason(work in local git though).
 
 int main(int argc, const char** argv) {
     int test_res = ctest_main(argc, argv);
